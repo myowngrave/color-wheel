bc674d0cbd6d2d4e795c6d2407e79fc89091b009
myowngrave <programmingd32@gmail.com>
2018-10-10 01:12:33 +0700

    init create-react-app

---
+++ docs/precache-manifest.b40732b89eb99522d2c8c82ac0238503.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "86fd07f209450cde6632",
+    "url": "/static/js/main.86fd07f2.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "86fd07f209450cde6632",
+    "url": "/static/css/main.cf9a0fc2.chunk.css"
+  },
+  {
+    "revision": "8ba76c4de31366c60a1bd85552fc02f1",
+    "url": "/index.html"
+  }
+];
