7fb071ceda4c40ea32631c6686bd982e275306c7
myowngrave <programmingd32@gmail.com>
2018-10-10 01:32:16 +0700

    Updates

---
+++ precache-manifest.d4a9afc0de0f94300620f0dad96be176.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/color-wheel/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fb7e50700f4868581ba1",
+    "url": "/color-wheel/static/js/runtime~main.fb7e5070.js"
+  },
+  {
+    "revision": "df9256e1ce7ff8418fe1",
+    "url": "/color-wheel/static/js/main.df9256e1.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/color-wheel/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "df9256e1ce7ff8418fe1",
+    "url": "/color-wheel/static/css/main.cf9a0fc2.chunk.css"
+  },
+  {
+    "revision": "873abf903a8a5652afe26d909c8cc2e9",
+    "url": "/color-wheel/index.html"
+  }
+];
