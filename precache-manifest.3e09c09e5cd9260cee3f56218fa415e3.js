225b0f0bb604f62f7572ccdcec3f82d57ee72e99
myowngrave <programmingd32@gmail.com>
2018-10-12 08:32:35 +0700

    Updates

---
+++ precache-manifest.3e09c09e5cd9260cee3f56218fa415e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb7e50700f4868581ba1",
+    "url": "/color-wheel/static/js/runtime~main.fb7e5070.js"
+  },
+  {
+    "revision": "c2b9f0ec943cc1e6943a",
+    "url": "/color-wheel/static/js/main.c2b9f0ec.chunk.js"
+  },
+  {
+    "revision": "3b7b784dbe54bdeefc9c",
+    "url": "/color-wheel/static/js/1.3b7b784d.chunk.js"
+  },
+  {
+    "revision": "c2b9f0ec943cc1e6943a",
+    "url": "/color-wheel/static/css/main.ff67fbf7.chunk.css"
+  },
+  {
+    "revision": "cd60624530101910e72f8d4d4d41bade",
+    "url": "/color-wheel/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/color-wheel/precache-manifest.da9188ee4e9a7f70a15451cfde5d4252.js"
+  "/color-wheel/precache-manifest.3e09c09e5cd9260cee3f56218fa415e3.js"
 );
 
 workbox.clientsClaim();
