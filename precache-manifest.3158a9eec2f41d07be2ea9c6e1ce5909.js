e326667ae9f42178a56a2f40f06c72dd5fb7b710
myowngrave <myowngrave1218@gmail.com>
2019-03-29 12:00:19 +0700

    Updates

---
+++ precache-manifest.3158a9eec2f41d07be2ea9c6e1ce5909.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66c2e963a90a0ef44069",
+    "url": "/color-wheel/static/css/main.99efa1c4.chunk.css"
+  },
+  {
+    "revision": "66c2e963a90a0ef44069",
+    "url": "/color-wheel/static/js/main.66c2e963.chunk.js"
+  },
+  {
+    "revision": "ca791d2c1f1a0adf1e17",
+    "url": "/color-wheel/static/js/runtime~main.ca791d2c.js"
+  },
+  {
+    "revision": "deed2549f9772187d849",
+    "url": "/color-wheel/static/js/2.deed2549.chunk.js"
+  },
+  {
+    "revision": "ec4c9962ba54eb91787aa93d361c10a8",
+    "url": "/color-wheel/static/media/Roboto-Black.ec4c9962.ttf"
+  },
+  {
+    "revision": "ee7b96fa85d8fdb8c126409326ac2d2b",
+    "url": "/color-wheel/static/media/Roboto-Bold.ee7b96fa.ttf"
+  },
+  {
+    "revision": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
+    "url": "/color-wheel/static/media/Roboto-Regular.3e1af3ef.ttf"
+  },
+  {
+    "revision": "fc84e998bc29b297ea20321e4c90b6ed",
+    "url": "/color-wheel/static/media/Roboto-Light.fc84e998.ttf"
+  },
+  {
+    "revision": "89e2666c24d37055bcb60e9d2d9f7e35",
+    "url": "/color-wheel/static/media/Roboto-Thin.89e2666c.ttf"
+  },
+  {
+    "revision": "50705c5ed1205b63efdbfee941a6b655",
+    "url": "/color-wheel/static/media/Roboto-BlackItalic.50705c5e.ttf"
+  },
+  {
+    "revision": "1eb7a893589ddce89d81cdb22a356660",
+    "url": "/color-wheel/static/media/Roboto-BoldItalic.1eb7a893.ttf"
+  },
+  {
+    "revision": "42bbe4eefcde1297b11dc4b6491e9746",
+    "url": "/color-wheel/static/media/Roboto-Italic.42bbe4ee.ttf"
+  },
+  {
+    "revision": "d1efcd4d126837fe0dcf9b6cf3a00d64",
+    "url": "/color-wheel/static/media/Roboto-LightItalic.d1efcd4d.ttf"
+  },
+  {
+    "revision": "bd19ad60600a1537c00d3b4923a5e5de",
+    "url": "/color-wheel/static/media/Roboto-MediumItalic.bd19ad60.ttf"
+  },
+  {
+    "revision": "d08840599e05db7345652d3d417574a9",
+    "url": "/color-wheel/static/media/Roboto-Medium.d0884059.ttf"
+  },
+  {
+    "revision": "0fc25386220a58203994ce45fb4ae570",
+    "url": "/color-wheel/static/media/Roboto-ThinItalic.0fc25386.ttf"
+  },
+  {
+    "revision": "2a4dc280d340d6264e006cfb7646f403",
+    "url": "/color-wheel/index.html"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.1.1/workbox-sw.js");
 
 importScripts(
-  "/color-wheel/precache-manifest.7c2c127303189ca54da11d299cdc77b6.js"
+  "/color-wheel/precache-manifest.3158a9eec2f41d07be2ea9c6e1ce5909.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/color-wheel/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/color-wheel/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
